a4bf59ae1a40e16997ad2784509cbc9250d03ec4
ZainChen <2384439266@qq.com>
2020-09-07 02:20:33 +0800

    update zain-ui web

--- component---src-pages-404-js-62748ce4fe38ebc3bfbd.js
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD:component---src-pages-404-js-12cc142e2074c8a451e3.js
-(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-12cc142e2074c8a451e3.js.map
-=======
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-62748ce4fe38ebc3bfbd.js.map
->>>>>>> f0cbd36fb6e5deb320250cb6a24c8013de858988:component---src-pages-404-js-62748ce4fe38ebc3bfbd.js
